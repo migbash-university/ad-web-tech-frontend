9695c322e32beb5ee0e6c608b2a7bc65fec7a48a
migbash <20924663+migbash@users.noreply.github.com>
2020-12-02 10:31:20 +0000

    Fixing the /fav_news

--- src/routes/profile/[user]/[view].svelte
+++ src/routes/profile/[user]/[view].svelte
@@ -9,6 +9,27 @@
 		// const { profile } = await api.get(`profiles/${username}`, user && user.token);
 		// return { profile, favorites: params.view === 'favorites' };
 	// }
+
+	let base = 'https://space-shadow-api.herokuapp.com/api';
+
+	if (process.env.NODE_ENV != 'production') {
+		base = 'http://127.168.1.0:8080/api';
+	}
+
+	import { news_data } from '../../../stores/dummy_news_data.js'
+
+	import { post_non_auth } from 'utils.js';
+
+	export async function preload({ params }, { user }) {
+		const uid = user.uid
+
+		const response = await post_non_auth(base + '/get_news_fav', { uid });
+		console.log(response)
+		// const { article } = await api.get(`articles/${params.slug}`, null);
+
+		return { response };
+	}
+
 </script>
 
 <script>
